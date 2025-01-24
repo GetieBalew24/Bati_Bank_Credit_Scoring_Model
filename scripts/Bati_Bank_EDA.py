bc6c671f036605b4fe440e187e3ee84718198bab
GetieBalew24 <getiebalew20@gmail.com>
2025-01-24 14:52:21 +0300

    Dataset overview and summary statistics

--- scripts/Bati_Bank_EDA.py
+++ scripts/Bati_Bank_EDA.py
@@ -33,3 +33,23 @@ class ExploratoryDataAnalysis:
         """
         logging.info("Fetching the first five rows of the DataFrame.")
         return self.df.head()
+    def dataset_overview(self):
+        """
+        Displays an overview of the dataset, including the number of rows,
+        number of columns, data types, and the first five rows.
+        """
+        logging.info("Displaying dataset overview.")
+        print("Dataset Overview:")
+        print(f"Number of rows: {self.df.shape[0]}")
+        print(f"Number of columns: {self.df.shape[1]}")
+        print("\nColumn Data Types:")
+        print(self.df.dtypes)
+        print("\nFirst 5 rows of the dataset:")
+        print(self.df.head())
+    def summary_statistics(self):
+        """
+        Displays summary statistics for numerical columns in the dataset.
+        """
+        logging.info("Calculating summary statistics.")
+        print("\nSummary Statistics:")
+        print(self.df.describe())
