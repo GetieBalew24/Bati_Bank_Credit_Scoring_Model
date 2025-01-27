7f94dec1ea0fb26f4f0ba7d66b57334b29e8c051
GetieBalew24 <getiebalew20@gmail.com>
2025-01-27 22:22:05 +0300

    RFMSRiskClassifier inintialize

---
+++ scripts/calculate_RFMS_score.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import os
+import logging
+
+# Create a 'log' folder if it doesn't exist
+if not os.path.exists('../log'):
+    os.makedirs('../log')
+
+# Set up logging configuration
+logging.basicConfig(
+    filename='../log/rfms_classifier.log',  # Log file path
+    level=logging.DEBUG,  # Set the logging level to DEBUG
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Log message format
+)
+
+class RFMSRiskClassifier:
+    def __init__(self, df):
+        """
+        Initialize the RFMSRiskClassifier with a DataFrame.
+        
+        Parameters:
+        - df: DataFrame containing transaction data with customer information.
+        """
+        self.df = df
+        logging.info('RFMSRiskClassifier initialized with dataframe of shape %s', df.shape)
+
+    def calculate_recency(self, current_date):
+        """
+        Calculate the recency (days since last transaction) for each customer.
+
+        Parameters:
+        - current_date: The current date used to calculate recency.
+
+        Returns:
+        - DataFrame with a new column 'Recency' representing the recency for each customer.
+        """
+        logging.info('Calculating recency...')
+        try:
+            self.df['TransactionStartTime'] = pd.to_datetime(self.df['TransactionStartTime'], errors='coerce').dt.tz_localize(None)
+            current_date = pd.to_datetime(current_date).tz_localize(None)
+            self.df['Recency'] = (current_date - self.df['TransactionStartTime']).dt.days
+            logging.info('Recency calculation successful, resulting dataframe shape: %s', self.df.shape)
+        except Exception as e:
+            logging.error('Error calculating recency: %s', e)
+        
+        return self.df
